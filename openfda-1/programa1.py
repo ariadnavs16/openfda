37a64c937f95d0f47e169a41696054f4d262dc13
alumno <ariadnaviera@hotmail.com>
2018-04-03 22:57:05 +0200

    Subo el programa1 con algunos cambios

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -1,36 +1,32 @@
-#He empleado un editor para poder ordenar los datos de la URL(esta en formato json).
-#El editor utilizado es "json editor online", nos permite estudiar el ficheron json y poder entender su estructura.
+#He empleado uneditor online (json editor online) para poder trabajar con la informacion de la URL, ya que esta en formato json.
 
-#Importamos los siguientes modulos, ya que seran necesarios para nuestro programa.
+#Importamos los siguientes modulos, ya que contienen funciones ya creadas en lenguaje python que seran necesarias para nuestro programa.
 import http.client
-import json #El modulo 'json' nos permite navegar por los ficheros json como si fueran listas o diccionarios.
+import json
 
-#Nuestro programa es un cliente de openfda, no es un servidor.
-headers={'User-Agent': 'http.client'}
+#Nuestro programa es cliente de openfda.
+headers={'User-Agent': 'http.client'} #En la cabecera indicamos el navegador que estamos utilizando para pedir la iformacion.
 
 
-connection = http.client.HTTPSConnection("api.fda.gov") #Establecemos conexion con el API de openfda.
-connection.request("GET", "/drug/label.json", None, headers) #Enviamos una peticion tipo "GET",
-                                                            #junto con el recurso ('/drug/label.json') que queremos solicitar
-respuesta= connection.getresponse() #Creamos una variable la cual tendra la respuesta de openfada.
+connection = http.client.HTTPSConnection("api.fda.gov") #Establecemos conexion con el API de openfda, es una funcion del modulo http.client.
+connection.request("GET", "/drug/label.json", None, headers) #Enviamos una peticion tipo "GET",junto con el recurso ('/drug/label.json') que queremos solicitar.
+respuesta= connection.getresponse() #Creamos una variable la cual tendra la respuesta de openfada (del servidor).
 
-info_completa = respuesta.read().decode('utf-8') #La variable 'info_completa' contiene la respuesta completa en formato json.
+info_completa = respuesta.read().decode('utf-8') #La variable 'info_completa' contiene la informacion de 'respuesta' en formato json.
 #Aplicamos el ".decode('utf-8')" por si en la informacion hay algun simbolo este se interprete correctamente.
 
 connection.close() #Cerramos la conexion.
 
 
 info_ordenada = json.loads(info_completa)#La variable 'info_ordenada' contiene la misma informacion pero ordenada.
-#Empleamos 'loads' para transformar en listas, diccionarios, etc. la informacion y que sea mas facil trabajar con ella.
+#Empleamos 'loads' para transformar en listas y diccionarios la informacion y que sea mas facil trabajar con ella.
 
-medicamento=info_ordenada['results'][0]#La iinformacion del medicamento se encuentra dentro de un diccionario cuya clave es result.
-                                       #Dentro del cual hay una lista de donde cogemos el primer valor (posicion 0).
+medicamento=info_ordenada['results'][0]#La informacion del medicamento se encuentra dentro de un diccionario cuya clave es 'results'.
+                                       #Dentro del cual hay una lista de donde cogemos el primer valor.
 
 print('Identificador(id):', medicamento['id']) #Para obtener la id del medicamento nos metemos en el diccionario de medicamento con la clave 'id'.
-                                              #Imprimimos dicha informacion, ya que nos la pide el enunciado.
 
 print('Proposito del producto', medicamento['purpose'][0]) #Para obtener el proposito realizamos el mismo porcedimiento que para el id, pero en este caso la clave del diccionario sera 'purpose'
 
-#Por ultimo, el fabricante se obtiene con la clave 'openfda' en cuyo interior hay otro diccionario,
-#nos metemos en la clave 'manufacturer_name, este contiene una lista, por ello cogemos la posicion 0 para obtener al fabricante.
+#Por ultimo, el fabricante se obtiene con la clave 'openfda' en cuyo interior hay otro diccionario en el que empleamos la clave 'manufacturer_name'y por ultimo la posicion 0 de la lista.
 print('Nombre del fabricante', medicamento['openfda']['manufacturer_name'][0])
